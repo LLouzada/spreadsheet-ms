ec4c6a71c8e66cecd8a72360221cf7a7e585c11c
LLouzada <loumad.soft@gmail.com>
2024-10-22 11:29:07 -0300

    microserviço funcionando (conda python 3.12)

--- app/main.py
+++ app/main.py
@@ -1,44 +1,46 @@
-import os
-import sys
+from datetime import datetime
+from flask import Flask, request, send_file
 import openpyxl
-from flask import Flask, request, jsonify
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-sys.path.insert(0, os.path.join(current_dir, 'lib', 'Flask-0.12'))
-sys.path.insert(0, os.path.join(current_dir, 'lib', 'openpyxl-1.7.0'))
+import os
 
 app = Flask(__name__)
 
-def generate_neogen_order(data):
-    # reads template file
-    template_file = os.path.join(current_dir, 'assets/templates', 'neogen_order_template.xlsx')
-    
-    wb = openpyxl.load_workbook(template_file)
-    amostrasSheet = wb['Amostras']
-    faturamentoSheet = wb['Faturamento']
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
+def generate_neogen_order(data):
+    # Lê o arquivo de template
+    template_file = os.path.join(current_dir, 'assets/templates', 'neogen_order_template.xlsx')
+    wb = openpyxl.load_workbook(template_file)
+    
+    faturamentoSheet = wb['Faturamento']
+    # Preencher as células de faturamento
     for item in data['data'][0]['faturamento']:
         cell = item['cell']
         value = item['value']
         faturamentoSheet[cell] = value
         
+    amostrasSheet = wb['Amostras']
+    # Preencher as células de amostras
     for item in data['data'][0]['amostra']:
         cell = item['cell']
         value = item['value']
         amostrasSheet[cell] = value
         
-    #return the file
-    file = os.path.join(current_dir, 'assets/files', 'neogen_order_nfa_' + data['filename'] + '_' + datetime.now().strftime("%Y%m%d%H%M%S") + '.xlsx')
-    wb.save(file)
-    return file
+    # Gerar o nome do arquivo e salvar o Excel gerado
+    filename = data['data'][0]['filename']
+    file_path = os.path.join(current_dir, 'assets/orders' , filename)
+    wb.save(file_path)
+    
+    return file_path
 
+# Endpoint para gerar e devolver o arquivo
 @app.route('/neogen_order', methods=['POST'])
 def generate_neogen_order_endpoint():
     data = request.get_json()
-    result = generate_neogen_order(data)
-    file = generate_neogen_order(data)
-    return jsonify(result)
+    file_path = generate_neogen_order(data)
+    
+    # Enviar o arquivo como resposta
+    return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
